011ed13c921ea2d1bd3e1eb65d62065ec93a2efd
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-05-07 18:10:41 +0300

    Закончил верстку хедера

--- header.php
+++ header.php
@@ -12,6 +12,41 @@
 </head>
 <body <?php body_class(); ?>>
 	<header>
+		<div class="fw-container">
+			<div class="fw-row">
+				<div class="fw-col-xs-12 fw-col-sm-6">
+					<div class="logo">
+						<a href="">
+							<img src="<?php echo get_template_directory_uri().'/images/logo.png' ?>">
+						</a>
+					</div>
+					<div class="header_contact_info">
+						<div class="slogan">Агроспецзакупка</div>
+						<div class="phone_header"><span class="hot_iline">Горячая линия:</span> 8-499-390-43-84</div>
+						<div class="email">info@agro-zakupka.com</div>
+					</div>	
+				</div>
+				<div class="fw-col-xs-12 fw-col-sm-6">
+					<div class="backet">
+						<a href="">0</a>
+					</div>
+					<div class="header_info">
+						<div class="info_d">Бесплатная доставка по всей<br> России и странам СНГ!</div>
+						<div class="header_widjet">
+							<div class="clock_action_header">до конца акции осталось:</div>
+							<div class="clock_action_body">
+								<div class="clock_h clock_elem">18<br><span>часов</span></div>
+								<div class="clock_d clock_elem">:</div>
+								<div class="clock_m clock_elem">32<br><span>минут</span></div>
+								<div class="clock_d clock_elem">:</div>
+								<div class="clock_s clock_elem">01<br><span>секунд</span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<?php
 			$args = array('theme_location' => 'top', 'container'=> 'nav', 'menu_class' => 'bottom-menu', 'menu_id' => 'bottom-nav');
 			wp_nav_menu($args);
