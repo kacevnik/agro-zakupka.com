1e43e5e271d59191101f720d919448bcd0ca6326
kacevnik <kacevnik@yandex.ru>
2017-05-09 00:24:11 +0300

    Закончил с главным меню (выпадающие списки)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-jQuery(document).ready(function ($) {		
-	$('.section_3').css({'background-position-x': ($(window).width() - 1200)/2 -365 + 'px'});
+jQuery(document).ready(function ($) {
+/*-------------------menu script------------------------------------------*/
 
-	$('.footer_menu ul').css({'margin-left': ($('.footer_menu').width()-$('.footer_menu ul').width())/2+'px'});
+	$('.sub-menu').parent('li').hover(function(){
+		$(this).find('.sub-menu').css({'min-width':$(this).width()+'px'});
+		$(this).find('.sub-menu').css({'margin-left':($(this).find('.sub-menu').width() - $(this).width()+1)/2*-1+'px'});
+		$(this).find('.sub-menu').stop(false, true).show();
+	}, function(){
+		$(this).addClass('activ_hover');
+		setTimeout(function(){
+			$('.activ_hover').find('.sub-menu').stop(false, true).hide();
+			$('.sub-menu').parent('li').removeClass('activ_hover');
+		},500);
+
+	});
+
+	$('.sub-menu').hover(function(){
+		$('.sub-menu').parent('li').removeClass('activ_hover');
+		$('.sub-menu').not(this).stop(false, true).hide();
+		$(this).stop(false, true).show();
+	}, function(){
+		$(this).stop(false, true).hide();
+	});
+
+/*------------------- end menu script------------------------------------------*/
+	
 });
